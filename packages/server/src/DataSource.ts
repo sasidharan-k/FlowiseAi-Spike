00c73cf890f14e63423d4c4343e8eb50f537cae7
Sasidharan k <sasidharan.k@elumitas.com>
2025-03-03 18:49:23 +0530

    ssl issue fix

--- packages/server/src/DataSource.ts
+++ packages/server/src/DataSource.ts
@@ -69,7 +69,9 @@ export const init = async (): Promise<void> => {
                 username: process.env.DATABASE_USER,
                 password: process.env.DATABASE_PASSWORD,
                 database: process.env.DATABASE_NAME,
-                ssl: getDatabaseSSLFromEnv(),
+                ssl: {
+                   rejectUnauthorized: false,
+                },
                 synchronize: false,
                 migrationsRun: false,
                 entities: Object.values(entities),
